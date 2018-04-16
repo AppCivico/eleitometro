d9381d1d97c7eff2fc25106c4623e13888027725
Laís Tomaz <lais@eokoe.com>
2018-04-16 18:49:29 -0300

    Enhance store to handle loading cards

--- src/store.js
+++ src/store.js
@@ -1,16 +1,37 @@
+/* eslint-disable no-param-reassign */
+
 import Vue from 'vue';
 import Vuex from 'vuex';
+import axios from 'axios';
+
+const api = 'http://159.89.51.41:1667';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    cards: [],
   },
   mutations: {
-
+    SET_PROGRAMS(state, { res }) {
+      state.cards = res;
+    },
   },
   actions: {
-
+    LOAD_CARDS({ commit }) {
+      return new Promise((resolve, reject) => {
+        axios.get(`${api}/cards`).then(
+          (response) => {
+            console.log('res', response);
+            commit('SET_CARDS', { res: response.data });
+            resolve();
+          },
+          (err) => {
+            reject(err.response);
+            console.error(err);
+          },
+        );
+      });
+    },
   },
 });
