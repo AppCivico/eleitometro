8ca605c28c9cb96791c8efa0975dc499ce09bac9
Laís Tomaz <lais@eokoe.com>
2018-04-16 18:35:32 -0300

    Create Card component

---
+++ src/components/Card.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="card">
+    <h1>Card</h1>
+    <h2>{{ content.title }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Card',
+  props: {
+    content: Object,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
