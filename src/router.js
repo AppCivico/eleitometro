b3552e6651839d8104bcb9ff3d08766e287dd8eb
Laís Tomaz <laisotomaz@gmail.com>
2018-05-01 18:47:18 -0300

    Add CardView to router

--- src/router.js
+++ src/router.js
@@ -2,6 +2,7 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
 import About from './views/About.vue';
+import CardView from './views/CardView.vue';
 
 Vue.use(Router);
 
@@ -17,6 +18,11 @@ export default new Router({
       path: '/about',
       name: 'about',
       component: About,
+    },
+    {
+      path: '/card/:id',
+      name: 'Resumo em',
+      component: CardView,
     },
   ],
 });
