d49406d3ca4a2531a4ca7cf5b7383c4f4952f210
Laís Tomaz <lais@eokoe.com>
2018-05-18 16:11:05 -0300

    Enhance Analyze component

--- src/views/Analyze.vue
+++ src/views/Analyze.vue
@@ -1,11 +1,55 @@
 <template>
-  <main>
-    <h2>Editorial</h2>
+  <main class="analyze">
+    <template v-if="analyze.length > 0">
+      <section class="analyze__description">
+        <svg class="svg-icon">
+          <use :xlink:href="`#${description.emojiSymbolId}`"></use>
+        </svg>
+        <h2>{{ description.name }}</h2>
+        <h3 v-if="type === 'candidate'">{{ description.politicalParty }}</h3>
+        <div v-html="description.description"></div>
+      </section>
+    </template>
+    <template>Sem informações</template>
   </main>
 </template>
 
 <script>
 export default {
-  name: 'Editorial',
+  name: 'Analyze',
+  computed: {
+    id() {
+      return this.$route.params.id;
+    },
+    analyze() {
+      return this.$store.state.analyze;
+    },
+    type() {
+      const arr = this.$route.path.split('/');
+      return arr[1];
+    },
+    description() {
+      if(this.analyze.length > 0) {
+        return this.analyze.find(item => item.type === 'description');
+      }
+    }
+  },
+  watch: {
+    id(val) {
+      this.getData(val);
+    }
+  },
+  mounted() {
+    this.getData(this.id);
+  },
+  methods: {
+    getData(id) {
+      const payload = {
+        id,
+        type: this.type,
+      }
+      this.$store.dispatch('LOAD_ANALYZE', payload);
+    },
+  },
 }
 </script>
