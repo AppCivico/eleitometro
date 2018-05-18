aaa2224ea3402b8ce8baefe33e426ff2fef0412d
Laís Tomaz <lais@eokoe.com>
2018-05-18 15:00:30 -0300

    Import svg script into main.js

--- src/main.js
+++ src/main.js
@@ -4,6 +4,10 @@ import router from './router';
 import store from './store';
 import './registerServiceWorker';
 
+// eslint-disable-next-line
+const __svg__ = { path: './assets/svg/*.svg', name: 'public/svg/[hash].svg' };
+require('webpack-svgstore-plugin/src/helpers/svgxhr')(__svg__);
+
 Vue.config.productionTip = false;
 
 new Vue({
