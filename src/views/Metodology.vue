0069e84b79f20b5580824b171d69e9ae9041c8d1
laistomazz <laisotomaz@gmail.com>
2018-05-24 11:40:35 -0300

    Create Metodology component

---
+++ src/views/Metodology.vue
@@ -0,0 +1,50 @@
+<template>
+  <main class="post">
+    <h3>Lorem ipsum dolor sit amet consectetur</h3>
+    <p>adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+
+    <h4>Section 1.10.32 of "de Finibus Bonorum et Malorum", written by Cicero in 45 BC</h4>
+    <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"</p>
+
+    <h4>1914 translation by H. Rackham</h4>
+    <p>"But I must explain to you how all this mistaken idea</p>
+
+    <p>of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. </p>
+  </main>
+</template>
+
+<script>
+export default {
+    name: 'Metodology'
+}
+</script>
+
+<style>
+.post {
+  padding: 0 15%;
+  color: #66757f;
+  text-align: left;
+}
+
+.post h3 {
+  color: #000;
+  font-size: 2.4em;
+  font-weight: 700;
+  margin-bottom: 30px;
+}
+
+.post h4 {
+  font-size: 1.8em;
+  font-weight: 700;
+  margin-bottom: 10px;
+}
+
+.post p,
+.post ul,
+.post ol {
+  font-size: 1.2em;
+  margin-bottom: 10px;
+}
+</style>
+
+
