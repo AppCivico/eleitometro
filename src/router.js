64e8dc387a55d68d8af51f3ae4922a21b1646415
laistomazz <laisotomaz@gmail.com>
2018-05-24 11:41:22 -0300

    Add About and Metodology to router

--- src/router.js
+++ src/router.js
@@ -3,6 +3,8 @@ import Router from 'vue-router';
 import Home from './views/Home.vue';
 import CardView from './views/CardView.vue';
 import Analyze from './views/Analyze.vue';
+import Metodology from './views/Metodology.vue';
+import About from './views/About.vue';
 
 Vue.use(Router);
 
@@ -32,6 +34,16 @@ export default new Router({
       path: '/panorama/:id',
       name: 'Panoramas',
       component: Analyze,
+    },
+    {
+      path: '/metodology',
+      name: 'Metodologia',
+      component: Metodology,
+    },
+    {
+      path: '/about',
+      name: 'Sobre',
+      component: About,
     },
   ],
 });
