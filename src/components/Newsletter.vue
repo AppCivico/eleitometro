737b6d28d16e7628d875be23a741a6eab4ba4729
laistomazz <laisotomaz@gmail.com>
2018-05-24 17:16:43 -0300

    Create Newsletter component

---
+++ src/components/Newsletter.vue
@@ -0,0 +1,27 @@
+<template>
+  <form>
+    <input type="email" name="email" v-model="email" required>
+    <p class="error" v-if="errorMessage !== ''">{{ errorMessage }}</p>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'Newsletter',
+  data() {
+    return {
+      loading: false,
+      email: '',
+      errorMessage: '',
+    };
+  },
+  methods: {
+    validateForm() {
+      if (this.email === '') {
+        this.errorMessage = 'Campo obrigatório';
+      }
+    },
+  }
+}
+</script>
+
