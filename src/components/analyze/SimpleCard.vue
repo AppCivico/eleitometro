2bdc17ca2680e3e8b93c4df83d8ca1ac29b0ac2a
Laís Tomaz <lais@eokoe.com>
2018-05-25 18:14:08 -0300

    Create SimpleCard component

---
+++ src/components/analyze/SimpleCard.vue
@@ -0,0 +1,21 @@
+<template>
+  <section
+    class="analyze__card"
+    v-if="content"
+    v-html="content.frontTHML"
+    :style="{backgroundColor: content.backgroundColor}"
+    >
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'SimpleCard',
+  props: {
+    content: Object,
+  },
+}
+</script>
+
+<style>
+</style>
