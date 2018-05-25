6b14f983bfb3d1e5807d983d7351c87b04f3187f
Laís Tomaz <lais@eokoe.com>
2018-05-25 17:18:06 -0300

    Add Description component

---
+++ src/components/analyze/Description.vue
@@ -0,0 +1,45 @@
+<template>
+  <section class="analyze__description" v-if="content.name">
+    <svg class="svg-icon">
+      <use :xlink:href="`#${content.emojiSymbolId}`"></use>
+    </svg>
+    <h2>{{ content.name }}</h2>
+    <h3 v-if="type === 'candidate'">{{ content.politicalParty }}</h3>
+    <div class="" v-html="content.description"></div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Description',
+  props: {
+    content: Object,
+    type: String,
+  },
+}
+</script>
+
+<style>
+.analyze__description {
+  padding: 0 15%;
+}
+
+.analyze__description svg {
+  width: 100%;
+}
+
+.analyze__description h2 {
+  font-size: 2.4em;
+  font-weight: 400;
+}
+
+.analyze__description h3 {
+  font-size: 1.6em;
+  color: #66757f;
+}
+
+.analyze__description div {
+  font-size: 2em;
+  font-weight: 300;
+}
+</style>
