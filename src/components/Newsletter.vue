83deeb98d5664eae2e86379e8f3a4aabc12ec91e
Laís Tomaz <lais@eokoe.com>
2018-05-25 12:46:56 -0300

    Create Newsletter component

---
+++ src/components/Newsletter.vue
@@ -0,0 +1,34 @@
+<template>
+  <form @submit.prevent="validateForm">
+    <input type="email" name="email" v-model="email">
+    <button type="submit">subscribe</button>
+    <p class="error">{{ errorMessage }}</p>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'Newsletter',
+  data() {
+    return {
+      loading: false,
+      errorMessage: '',
+      email: '',
+    };
+  },
+  methods: {
+    validateForm() {
+      const { email } = this;
+      if (email === '') {
+        this.errorMessage = 'Campo obrigatório';
+      } else {
+        this.subscribe(email);
+      }
+    },
+    subscribe(email) {
+      console.log(email);
+    }
+  }
+}
+</script>
+
