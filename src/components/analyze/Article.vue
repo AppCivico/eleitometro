3d9d3a91d79a9ac751c918bb47b2e08647d529a1
Laís Tomaz <lais@eokoe.com>
2018-05-25 20:15:52 -0300

    Edit Article

--- src/components/analyze/Article.vue
+++ src/components/analyze/Article.vue
@@ -1,11 +1,14 @@
 <template>
   <section class="analyze__article" v-if="content">
     <span class="article__writer">ANÁLISE FGV DAPP</span>
-    <h3 v-html="content.title"></h3>
-    <img :src="content.image" alt="article thumbnail" v-if="mediaType === 'image'">
+    <!--<h3 v-html="content.title"></h3>-->
+    <h3><strong>Jair Bolsonaro</strong> e as mídias sociais</h3>
+    <img src="https://www.efsa.europa.eu/sites/default/files/All%20staff%20picture%20new.jpg" alt="remove">
+    <!-- <img :src="content.image" alt="article thumbnail" v-if="mediaType === 'image'">
     <div class="video__wrapper">
       <iframe width="560" height="315" :src="`https://www.youtube.com/embed/${videoId}?rel=0&amp;controls=0&amp;showinfo=0`" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
     </div>
+    -->
     <div class="article__content" v-html="content.description"></div>
     <a :href="content.link" targe="_blank">LER MATÉRIA</a>
   </section>
@@ -45,11 +48,14 @@ export default {
 }
 
 .analyze__article > h3,
-.analyze__article > img,
 .analyze__article .article__content,
 .analyze__article .article__writer,
 .analyze__article > a {
   padding: 0 30px;
+}
+
+.analyze__article > img {
+  margin-bottom: 10px;
 }
 
 .analyze__article h3 {
