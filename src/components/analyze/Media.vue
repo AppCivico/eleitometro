15fd3b46193e9dbdc8c105d9cdcd60e4e0d860a9
Laís Tomaz <lais@eokoe.com>
2018-05-25 19:51:26 -0300

    Enhance Media style

--- src/components/analyze/Media.vue
+++ src/components/analyze/Media.vue
@@ -2,8 +2,10 @@
   <section class="analyze__media" v-if="content">
     <h3 v-html="content.title"></h3>
     <img :src="content.image" alt="article thumbnail" v-if="mediaType === 'image'">
-    <iframe width="560" height="315" :src="`https://www.youtube.com/embed/${videoId}?rel=0&amp;controls=0&amp;showinfo=0`" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
-    <div v-html="content.description"></div>
+    <div class="video__wrapper">
+      <iframe width="560" height="315" :src="`https://www.youtube.com/embed/${videoId}?rel=0&amp;controls=0&amp;showinfo=0`" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+    </div>
+    <div class="media__content" v-html="content.description"></div>
   </section>
 </template>
 
@@ -28,4 +30,48 @@ export default {
 </script>
 
 <style>
+.analyze__media {
+  background: #fff;
+  padding: 20px 0;
+  margin-bottom: 30px;
+  border-radius: 14px;
+  text-align: left;
+}
+
+.analyze__media > h3,
+.analyze__media > img,
+.analyze__media .media__content {
+  padding: 0 30px;
+}
+
+.analyze__media h3 {
+  font-size: 2.4em;
+  color: #66757f;
+  font-weight: 300;
+  margin-bottom: 20px;
+}
+
+.analyze__media h3 strong {
+  font-weight: 700;
+}
+
+.analyze__media .media__content {
+  font-size: 1.4em;
+}
+
+.video__wrapper {
+  position: relative;
+  padding-bottom: 56.25%;
+  padding-top: 25px;
+  height: 0;
+  margin-bottom: 20px;
+}
+
+.video__wrapper iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
 </style>
