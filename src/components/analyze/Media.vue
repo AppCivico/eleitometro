b4bb6556fbdc5244fc362df27870e6046c1ebb79
Laís Tomaz <lais@eokoe.com>
2018-05-25 18:14:02 -0300

    Create Media component

---
+++ src/components/analyze/Media.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="analyze__media" v-if="content">
+    <h3 v-html="content.title"></h3>
+    <div v-html="content.description"></div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'Media',
+  props: {
+    content: Object,
+  },
+}
+</script>
+
+<style>
+</style>
