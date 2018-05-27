4b9428716ec79ee790d1761e6aff654f9eca4813
laistomazz <laisotomaz@gmail.com>
2018-05-26 23:43:40 -0300

    Fix identation again rs

--- src/views/CardView.vue
+++ src/views/CardView.vue
@@ -1,57 +1,57 @@
 <template>
-	<main class="cardView">
-		<div class="cardView__wrapper">
-			<Card v-if="card.id" :content="card" :width="100"/>
-			<p v-else class="not-found">Esse card não foi encontrado :(</p>
-		</div>
-		<router-link to="/" class="cardView__button">Acessar Eleitômetro</router-link>
-	</main>
+  <main class="cardView">
+    <div class="cardView__wrapper">
+      <Card v-if="card.id" :content="card" :width="100"/>
+      <p v-else class="not-found">Esse card não foi encontrado :(</p>
+    </div>
+    <router-link to="/" class="cardView__button">Acessar Eleitômetro</router-link>
+  </main>
 </template>
 
 <script>
 import Card from '../components/Card.vue';
 
 export default {
-	name: 'CardView',
-	components: {
-    Card,
+  name: 'CardView',
+  components: {
+  Card,
   },
   mounted() {
-	  this.$store.dispatch('LOAD_CARD', this.$route.params.id);
+    this.$store.dispatch('LOAD_CARD', this.$route.params.id);
   },
   computed: {
     card() {
-			return this.$store.state.card;
-		},
+      return this.$store.state.card;
+    },
   },
 }
 </script>
 
 <style lang="scss" scoped>
 .cardView__wrapper {
-	padding: 0 2%;
+  padding: 0 2%;
 }
 
 .cardView__wrapper .not-found {
-	font-size: 2em;
+  font-size: 2em;
 }
 
 .cardView__wrapper .card {
-	float: none;
+  float: none;
 }
 
 .cardView__button {
-	font-size: 2em;
-	color: $white;
-	text-decoration: none;
-	text-align: left;
-	background-color: $gray;
-	background-image: url('../assets/icon-arrow-right.png');
-	background-repeat: no-repeat;
-	background-position: center right 20px;
-	display: block;
-	padding: 20px;
-	margin-top: 20px;
+  font-size: 2em;
+  color: $white;
+  text-decoration: none;
+  text-align: left;
+  background-color: $gray;
+  background-image: url('../assets/icon-arrow-right.png');
+  background-repeat: no-repeat;
+  background-position: center right 20px;
+  display: block;
+  padding: 20px;
+  margin-top: 20px;
 }
 </style>
 
