496f9bba8e56d30fbf7f7eb36704e65891f349cd
laistomazz <laisotomaz@gmail.com>
2018-06-13 12:49:41 -0300

    Use h4 instead of h3 in Metodology

--- src/views/Metodology.vue
+++ src/views/Metodology.vue
@@ -1,12 +1,12 @@
 <template>
   <main class="post">
-    <h3>Sobre</h3>
+    <h4>Sobre</h4>
     <p>A ferramenta exibe de forma rápida, prática e harmoniosa detalhes sobre o debate eleitoral no Twitter em tempo real, explicitando informações sobre temas e candidatos em destaque na corrida eleitoral, presença de interferências ilegítimas e casos de discursos de ódio (homofobia, machismo, racismo, regionalismo, xenofobia, entre outras ofensas). As análises apresentadas são obtidas após uma sequência de procedimentos metodológicos aplicados pela equipe FGV DAPP.</p>
     
-    <h3>Procedimentos metodológicos</h3>
+    <h4>Procedimentos metodológicos</h4>
     <p>Os dados são adquiridos através de coleta feita em tempo real na API do Twitter e armazenados em um banco de dados interno para que sejam processados. O processamento inclui cálculos como acúmulo do volume de menções, percentuais e variações percentuais diárias dos dados. Por fim, a análise dos resultados é realizada manualmente pela equipe de pesquisadores através da criação de cards com textos explicativos.</p>
     
-    <h3>Cards</h3>
+    <h4>Cards</h4>
     <p>Os cards buscam apresentar de forma transparente o panorama resumido sobre os principais temas, candidatos e palavras de ódio citadas no debate nas redes. Para isso, contam com análises gráficas de evolução temporal por hora e por dia, volume acumulado de menções por categoria no dia, percentual de participação de robôs na semana e contribuição de cada palavra nos discursos de ódio gerados nas redes no contexto eleitoral naquela semana.</p>
   </main>
 </template>
